339e5286508970ff2849d1f629f133d0d27effe8
Yusuf Anugrah Putra Aditama <yapaditama@gmail.com>
2023-08-30 01:03:27 +0700

    Save Progress - #3

--- project/vue-firebase/app.js
+++ project/vue-firebase/app.js
@@ -1,15 +1,46 @@
 const app = Vue.createApp({
     data(){
         return {
+            url: 'http://www.cuppyzh.dev',
             showBooks: true,
-            title: 'Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            books: [
+                {
+                    title: 'book 1', author: 'author 1', img: 'assets/image-1.jpg', isFav: true
+                },
+                {
+                    title: 'book 2', author: 'author 2', img: 'assets/image-2.jpg', isFav: false
+                },
+                {
+                    title: 'book 3', author: 'author 3', img: 'assets/image-3.png', isFav: true
+                }
+            ]
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+
+            if (data){
+                console.log(data)
+            }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        changeFav(book){
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        hello() {
+            return 'helloooooooooooooo'
+        },
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
