54044a65152328bc7a285ceeba55e1ff9e5f5713
Yusuf Anugrah Putra Aditama <yapaditama@gmail.com>
2023-09-01 01:29:38 +0700

    Save Progress
    - New vue project
    - Move old project

---
+++ project/modal-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return{
+      title: './cuppyzh.dev'
+    }
+  }
+}
+</script>
+
+<!-- This style is global -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #F00;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
